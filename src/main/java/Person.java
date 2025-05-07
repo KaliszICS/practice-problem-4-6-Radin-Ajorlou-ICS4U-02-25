8486187062584621596c8e3979fe1ac3d9f4e13b
Radin-Ajorlou-ICS4U-02-25 <radin.ajorlou@student.tdsb.on.ca>
2025-05-07 14:20:28 +0000

    everything except addSibling done

---
+++ src/main/java/Person.java
@@ -0,0 +1,53 @@
+class Person{
+    private String firstname;
+    private String lastname;
+    private int birthDay;
+    private int birthMonth;
+    private int birthYear;
+    private String ssn;
+    private int numberOfSiblings;
+    private Person[] siblings;
+
+    public Person(String firstname, String lastname, int birthDay, int birthMonth, int birthYear, String ssn){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthDay = birthDay;
+        this.birthMonth = birthMonth;
+        this.birthYear = birthYear;
+        this.ssn = ssn;
+        this.numberOfSiblings = 0;
+    }
+
+    public String getFirstname(){
+        return firstname;
+    }
+
+    public String getLastname(){
+        return lastname;
+    }
+
+    public void setFirstname(String firstname){
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname){
+        this.lastname = lastname;
+    }
+
+    public String getBirthday(){
+        return birthDay + "/" + birthMonth + "/" + birthYear;
+    }
+
+    public void addSibling(Person sibling){
+        Person[] newSiblings = new Person[this.siblings.length + 1];
+        for (int i = 0; i < newSiblings.length; i++){
+            newSiblings[i] = siblings[i];
+        }
+        newSiblings[newSiblings.length - 1] = sibling.clone();
+        this.siblings = newSiblings;
+        this.numberOfSiblings++;
+    }
+
+    @Override 
+    public 
+}
